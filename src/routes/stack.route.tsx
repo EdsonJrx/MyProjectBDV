c2a380774be5c593b5d2db3685c627d2ca4f0679
EdsonJrx <edinhoa7x@gmail.com>
2023-06-10 01:43:42 -0400

    correção stack route

--- src/routes/stack.route.tsx
+++ src/routes/stack.route.tsx
@@ -1,6 +1,5 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {LoginScreen} from '../pages/stack/login'
-import { TabRoutes } from './tab.route';
 import { DrawerRoutes } from './drawer.route';
 
 const Stack = createNativeStackNavigator();
