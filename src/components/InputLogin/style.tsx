13f04da6ba00e297af1b5e098482ea561ff7ff1c
EdsonJrx <edinhoa7x@gmail.com>
2023-07-03 11:32:44 -0400

    funcionando

--- src/components/InputLogin/style.tsx
+++ src/components/InputLogin/style.tsx
@@ -1,18 +1,11 @@
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
-    width:30px;
-    height:30px;
-    border-radius:15px;
-    background-color:"#3eccff";
-    align-items:'center';
-    justify-content:'center';
-    margin-right:20px;
+    
 `
 export const Button = styled.TouchableOpacity`
     
 `
 export const Text = styled.Text`
-    width:100px;
-    font-size:20px
+
 `
